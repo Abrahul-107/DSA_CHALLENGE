0fe4282dbc09c3b59d370c0a78619b442796f193
Rahul <abrahul882@gmail.com>
2024-02-17 10:44:43 +0530

    Product of array except self

--- Array/08_product_array_except_itself.cpp
+++ Array/08_product_array_except_itself.cpp
@@ -1,22 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<int> productOfArrayExceptItself(int *arr,int n)
-{
-    vector<int>ans;
-    int prod = 1;
-    for(int i=0;i<n;i++)
-        prod *= arr[i];
-    
-    for(int i=0;i<n;i++)
-        ans.push_back(prod/arr[i]);
-    return ans;
-}
+ vector<int> productExceptSelf(int *arr,int n) {
+        
+        vector<int> ans(n);
+        ans[0] = 1;
+        for(int i=1; i<n; i++)
+        {
+            ans[i] = ans[i-1] * arr[i-1];
+        }
+        int right = 1;
+        for(int i=n-1; i>=0; i--)
+        {
+            ans[i] *= right;
+            right *= arr[i];
+        }
+        return ans;
+ }
 int main()
 {
-    int arr[] = {1,2,3,4,5};
+    int arr[] = {1,2,0,4,5};
     int n = sizeof(arr)/sizeof(arr[0]);
-    vector<int>v = productOfArrayExceptItself(arr,n);
+    vector<int>v = productExceptSelf(arr,n);
     for(auto it:v)
         cout<<it<<" ";
 
