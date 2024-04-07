1bf2c9034239abe9d50779bb2a19a0bd064d7f5a
Abrahul-107 <abrahul882@gmail.com>
2024-04-07 21:53:48 +0530

    Ferrish wheel CSES solution

---
+++ Array/15_ferrisWheel.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,k;
+    vector<int>weight(n);
+
+    for(int i=0;i<n;i++)
+        cin>>weight[i];
+
+    sort(weight.begin(),weight.end());
+    int i=0,j = n-1;
+    int ans = 0;
+
+    while(i<=j)
+    {
+        if(weight[i]+weight[j]>k)
+            j--;
+        else
+            i++,j--;
+        ans++;
+    }
+
+    cout<<ans<<" ";
+
+    return 0;
+}
