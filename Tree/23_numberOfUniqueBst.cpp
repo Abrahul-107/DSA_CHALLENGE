968536d1985c97487e9fa288409ee7d7de02b2dc
Abrahul-107 <Abrahul-107>
2024-05-31 00:03:14 +0530

    Unique bst

--- Tree/23_numberOfUniqueBst.cpp
+++ Tree/23_numberOfUniqueBst.cpp
@@ -1,55 +1,60 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class TreeNode
-{
-
-
+class TreeNode {
 public:
-	int data;
-	TreeNode *left;
-	TreeNode *right;
+    int data;          
+    TreeNode *left;    
+    TreeNode *right;   
 
-	TreeNode(int val):data(val),left(nullptr),right(nullptr){};
-
+    TreeNode(int val) : data(val), left(nullptr), right(nullptr) {}
 };
 
-int numTreeRecursion(int n)
-{
-	if(n<=1)
-		return 1;
-	int ans = 0;
+int numTreeRecursion(int n) {
+    // Base case: If there are 0 or 1 nodes, there is exactly one BST possible
+    if (n <= 1)
+        return 1;
 
-	for(int i=1;i<=n;i++)
-	{
-		ans += numTreeRecursion(i-1)*numTreeRecursion(n-i);
-	}
-	return ans;
+    int ans = 0;
+    
+    // Recursively calculate the number of unique BSTs for each possible root
+    for (int i = 1; i <= n; i++) {
+        // The number of unique BSTs with `i` as the root is the product of the number
+        // of unique BSTs in the left (i-1 nodes) and right (n-i nodes) subtrees
+        ans += numTreeRecursion(i - 1) * numTreeRecursion(n - i);
+    }
+    
+    return ans;
 }
 
-int numTreeMemo(int n,vector<int>&dp)
-{
-	if(n<=1)
-		return 1;
-	if(dp[n]!= -1)
-		return dp[n];
-	int res = 0;
+int numTreeMemo(int n, vector<int>& dp) {
+    // Base case: If there are 0 or 1 nodes, there is exactly one BST possible
+    if (n <= 1)
+        return 1;
 
-	for(int i=1;i<=n;i++)
-	{
-		res += numTreeMemo(i-1,dp)*numTreeMemo(n-i,dp);
-	}
-	return dp[n] = res;
+    // Return the stored result if it has already been computed
+    if (dp[n] != -1)
+        return dp[n];
 
+    int res = 0;
+    
+    // Recursively calculate the number of unique BSTs for each possible root
+    for (int i = 1; i <= n; i++) {
+        // The number of unique BSTs with `i` as the root is the product of the number
+        // of unique BSTs in the left (i-1 nodes) and right (i+1....n nodes) i:e (n-i) subtrees
+        res += numTreeMemo(i - 1, dp) * numTreeMemo(n - i, dp);
+    }
+    
+    // Store the computed result in dp array
+    return dp[n] = res;
 }
 
+int main() {
+    int n = 3;  
+    vector<int> dp(n + 1, -1); 
 
-int main()
-{
-	int n =3;
-	vector<int>ans;
-	cout<<numTreeRecursion(n);
-	cout<<numTreeMemo(n,ans);
+    cout << "Number of unique BSTs (recursion): " << numTreeRecursion(n) << endl;
+    cout << "Number of unique BSTs (memoization): " << numTreeMemo(n, dp) << endl;
 
-	return 0;
+    return 0;
 }
