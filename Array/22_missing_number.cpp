a8660399a1147932869259fb4b44b1d2a033b1da
Abrahul-107 <Abrahul-107>
2024-05-30 23:24:38 +0530

    Missing number in array

---
+++ Array/22_missing_number.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int findMissing(int *arr,int n)
+{
+    int ans = 0;
+    for(int i=0;i<n;i++)
+        ans = ans ^ arr[i] ^ i;
+    return ans^n;
+}
+int main()
+{
+    int arr[] = {0,1,3};
+    int n = 3;
+    int ans = findMissing(arr,n);
+
+    cout<<ans;
+    return 0;
+}
