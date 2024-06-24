84ae79dbefa1afcd36445fb709ae672661dd110b
rahulparidazessta <rahul.parida@zessta.com>
2024-06-24 14:55:51 +0530

    Return kth largest elements

--- Heap/04_kthLargest.cpp
+++ Heap/04_kthLargest.cpp
@@ -1,6 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//Using maxHeap for largest
+int kthLargestNumber(vector<int>&arr,int k)
+{
+    priority_queue<int>pq;
+    int n = arr.size();
+
+    for(int i=0;i<n;i++)
+        pq.push(arr[i]);
+    k = k-1;
+    while(k--)
+        pq.pop();
+    return pq.top();
+
+}
+
+//Using minHeap for Largest
 vector<int> kthLargestReturn(vector<int>&arr,int k)
 {
     priority_queue<int, vector<int>, greater<int>> minHeap;
@@ -30,6 +46,7 @@ int main()
     vector<int>ans = kthLargestReturn(arr,k);
     for(int i=0;i<k;i++)
         cout<<ans[i]<<" ";
+    cout<<"Kth largest number is "<<kthLargestNumber(arr,k)<<endl;
 
     return 0;
 
