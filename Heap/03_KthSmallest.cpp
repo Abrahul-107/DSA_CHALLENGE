fdf14e8152e68c10cbf32617c645731414236dfe
rahulparidazessta <rahul.parida@zessta.com>
2024-06-24 15:01:11 +0530

    Kth smallest using minHeap and maxHeap

--- Heap/03_KthSmallest.cpp
+++ Heap/03_KthSmallest.cpp
@@ -1,6 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//Using minHeap for kth smallest
+int kthSmallestUsingMinhheap(vector<int>&arr,int k)
+{
+    priority_queue<int, vector<int>,greater<int>>minHeap;
+    int n = arr.size();
+    for(int i=0;i<n;i++)
+        minHeap.push(arr[i]);
+    k = k-1;
+    while(k--)
+        minHeap.pop();
+    return minHeap.top();
+}
+
+//Using maxHeap for kth smallest
 int kthSmallest(vector<int>&arr,int k)
 {
     priority_queue<int>maxHeap;
@@ -19,7 +33,7 @@ int main()
 {
     vector<int>arr = {1,4,2,9,7,5};
     int k = 3;
-    int ans = kthSmallest(arr,k);
+    int ans = kthSmallestUsingMinhheap(arr,k);
     cout<<ans;
 
 
