d9644b0c9fd2edfa33224886ba00ab9ba520c8e8
Abrahul-107 <Abrahul-107>
2024-06-28 18:43:35 +0530

    Find closest K elements

--- Heap/07_findKclosest.cpp
+++ Heap/07_findKclosest.cpp
@@ -1,33 +1,37 @@
-//https://leetcode.com/problems/find-k-closest-elements/description
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<int>findClosestElements(vector<int>&arr,int k,int x)
+vector<int> findClosestElements(vector<int>& arr, int k, int x)
 {
-    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
     int n = arr.size();
-    for(int i=0;i<n;i++)
-        pq.push({abs(x-arr[i]) , arr[i]});
     
-    vector<int>ans;
+    for(int i = 0; i < n; i++)
+        pq.push({abs(x - arr[i]), arr[i]});
+    
+    vector<int> ans;
     while (k--)
     {
-        arr.push_back(pq.top().second);
+        ans.push_back(pq.top().second);
         pq.pop();
     }
-    sort(ans.begin(),ans.end());
+    
+    sort(ans.begin(), ans.end());
 
     return ans;
-    
 }
 
 int main()
 {
-    vector<int>arr = {1,2,3,4,5};
-    int k=4 ;
+    vector<int> arr = {1, 2, 3, 4, 5};
+    int k = 4;
     int x = 3;
-
-
+    vector<int> ans = findClosestElements(arr, k, x);
+    
+    // Print the result
+    for(auto it : ans)
+        cout << it << " ";
+    cout << endl;
 
     return 0;
 }
